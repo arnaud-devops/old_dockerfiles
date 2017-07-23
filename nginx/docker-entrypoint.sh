227a26ac283ab4e5eab82ad8ae1f21708859d6de
Arnaud Astruc <oxynux@gmail.com>
2017-07-23 13:52:56 +0200

    no need to chown /tmp

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'nginx' -a "$(id -u)" = '0' ]; then
     touch /var/run/nginx.pid
     touch /var/run/nginx.lock
-    chown -R nginx:nginx /etc/nginx /var/run/nginx.pid /var/run/nginx.lock /tmp
+    chown -R nginx:nginx /etc/nginx /var/run/nginx.pid /var/run/nginx.lock
     exec su-exec nginx:nginx "$@"
 fi
 
