5841358de1fd65d854c3fd3e50a3e5fffb56797f
Arnaud Astruc <arnaud.astruc@earthcube.eu>
2017-12-08 17:50:14 +0100

    alpine 3.7 nginx 1.13.7

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'nginx' -a "$(id -u)" = '0' ]; then
     touch /var/run/nginx.pid
     touch /var/run/nginx.lock
     chown -R nginx:nginx /etc/nginx /var/run/nginx.pid /var/run/nginx.lock
-    exec su-exec nginx:nginx "$@"
+    exec su-exec nginx:nginx "$0" "$@"
 fi
 
 exec "$@"
